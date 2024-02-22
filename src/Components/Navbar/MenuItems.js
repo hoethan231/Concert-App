f69eaa37fc4be3a40b3480fa8cb76b6070475596
Amy Okuma <amyokuma@gmail.com>
2024-02-22 00:51:27 -0800

    feat: add nav bar

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: "CONCERTS",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "ABOUT",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "LOG IN",
+        url: "#",
+        cName: "nav-links-mobile"
+    }
+]
