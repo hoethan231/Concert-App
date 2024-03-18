e898f5411f2d1eedd8951b97244b43a64b88d234
Ling Tang <happylingtang@gmail.com>
2024-03-18 10:49:30 -0700

    test update

--- frontend/src/Pages/AboutUs/AboutUs.js
+++ frontend/src/Pages/AboutUs/AboutUs.js
@@ -4,7 +4,18 @@ import "./AboutUs.css"
 function AboutUs() {
 
     return (
-        <h1>Hello from AboutUs</h1>
+        <div className="about-page">
+            <div className="image">
+
+            </div>
+        <div className="about-us">
+            <h1 className="tagline">HELLO!</h1>
+        <p className= "description">
+            We are a small team based at SJSU that developed this project for a kickstarter program.
+             This is all of our first full stack project so it was difficult but was overall a great learning experience.
+              We hope you enjoy!</p>
+              </div>
+        </div>
     );
 
 }
